28469eb5f77484e2f8d492d2b7324b3896b81d8a
josh nickson <jnickson1506@gmail.com>
2018-10-08 13:09:53 +0100

    add history feature in account

--- lib/account.rb
+++ lib/account.rb
@@ -1,15 +1,17 @@
 class Account
-  attr_accessor :balance
-
+  attr_reader :balance, :history
   def initialize
     @balance = 0
+    @history = []
   end
 
   def deposit(amount)
     @balance += amount
+    @history << {credit: amount, balance: @balance}
   end
 
   def withdraw(amount)
     @balance -= amount
+    @history << {debit: amount, balance: @balance}
   end
 end
