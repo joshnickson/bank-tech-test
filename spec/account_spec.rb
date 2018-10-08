aaf9652807febdbdd2af8c3ee26fd9048b0c389d
josh nickson <jnickson1506@gmail.com>
2018-10-08 12:44:24 +0100

    formalise feature tests (not IRB) and refactor tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,14 +3,14 @@ require 'account'
 describe Account do
   let(:account) { described_class.new }
 
-  describe 'Deposit funds' do
-    it 'allows user to deposit funds' do
+  describe '#deposit' do
+    it 'adds amount to balance' do
       expect { account.deposit(100) }.to change { account.balance }.by(100)
     end
   end
 
-  describe 'Withdraw funds' do
-    it 'allows user to withdraw funds' do
+  describe '#withdraw' do
+    it 'deducts amount to balance' do
       expect { account.withdraw(100) }.to change { account.balance }.by(-100)
     end
   end
---
+++ spec/features/account_feature_spec.rb
@@ -0,0 +1,16 @@
+require 'account'
+
+describe "Account feature tests" do
+  let(:account) { Account.new }
+
+  describe 'Deposit funds' do
+    it 'allows user to deposit funds' do
+      account.deposit(1000)
+      expect(account.balance).to eq(1000)
+    end
+    it 'allows user to withdraw funds' do
+      account.withdraw(1000)
+      expect(account.balance).to eq(-1000)
+    end
+  end
+end
