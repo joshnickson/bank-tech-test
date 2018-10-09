569d98bac241d4fc9769371efbf051ff8abebf3e
josh nickson <jnickson1506@gmail.com>
2018-10-09 17:43:58 +0100

    add tests for amount edge cases in account class

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,9 +1,13 @@
 require 'account'
-
 describe Account do
-  let(:history) { double :history, log_deposit: nil, log_withdrawal: nil, show_statement: "statement" }
+  let(:history) do
+    double :history,
+    log_deposit: nil,
+    log_withdrawal: nil,
+    show_statement: 'statement'
+  end
   let(:account) { described_class.new(history) }
-  
+
   describe '#new' do
     it 'instantiates with balance of zero' do
       expect(account.balance).to eq 0
@@ -17,6 +21,12 @@ describe Account do
     it 'adds amount to balance' do
       expect { account.deposit(100) }.to change { account.balance }.by(100)
     end
+    it 'throws error if amount less that zero' do
+      expect { account.deposit(-100) }.to raise_error('Amount must be greater than zero')
+    end
+    it 'throws error if amount not a number' do
+      expect { account.deposit('100') }.to raise_error('Please enter a valid amount')
+    end
   end
 
   describe '#withdraw' do
@@ -24,11 +34,17 @@ describe Account do
       account.deposit(100)
       expect { account.withdraw(100) }.to change { account.balance }.by(-100)
     end
+    it 'throws error if amount less that zero' do
+      expect { account.withdraw(-100) }.to raise_error('Amount must be greater than zero')
+    end
+    it 'throws error if amount not a number' do
+      expect { account.withdraw('100') }.to raise_error('Please enter a valid amount')
+    end
   end
 
   describe '#view_statement' do
     it 'prints bank statement to the console' do
-      expect { account.view_statement }.to output("statement\n").to_stdout 
+      expect { account.view_statement }.to output("statement\n").to_stdout
     end
-  end 
+  end
 end
